11b945d832b5ead90218e7b8901863fbfae55dfe
Anda BEN <anben@c5r6s3.wethinkcode.co.za>
2019-10-18 14:33:55 +0200

    adding

--- includes/login_inc.php
+++ includes/login_inc.php
@@ -2,6 +2,7 @@
 $username = $_POST['username'];
 $password = $_POST['password'];
 
+
 echo "User: ".$username."<br>";
 echo "Password: ".$password;
 ?>
--- includes/signup_inc.php
+++ includes/signup_inc.php
@@ -1,14 +1,46 @@
 <?php
-$email = $_POST['email'];
-$fullname = $_POST['name'];
-$username = $_POST['username'];
-$password = $_POST['password'];
-$repassword = $_POST['repassword'];
+function validPass($password)
+{
+    if(strlen($password) >= 8){
+        if(!ctype_alpha($password) && !ctype_lower($password)){
+            return TRUE;
+        }
+    }
+}
+if (isset($_POST['signup']))
+{
+    $email = $_POST['email'];
+    $fullname = $_POST['name'];
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $repassword = $_POST['repassword'];
+
+    if (empty($email) || empty($fullname) 
+    || empty($username) || empty($password) || empty($repassword))
+    {
+        header("Location: ../signup.php?error=emptyfields");
+        exit();
+    }
+    elseif ($password !== $repassword)
+    {
+        header("Location: ../signup.php?error=passwordnotmatching");
+        exit();
+    }
+    elseif (!filter_var($email, FILTER_VALIDATE_EMAIL))
+    {
+        header("Location: ../signup.php?error=invalidmail");
+        exit();
+    }
+    elseif (validPass($password) !== TRUE)
+    {
+        header("Location: ../signup.php?error=invalidpassword");
+        exit();
+    }
 
 echo "mail:" .$email. "<br>";
 echo "name:" .$fullname. "<br>";
 echo "user:" .$username. "<br>";
 echo "password:" .$password. "<br>";
 echo "repassword:" .$repassword;
-
+}
 ?>
