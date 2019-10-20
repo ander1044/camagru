372c610987d7d7cc7956c1f3b0845dbb14492dd6
root <root@localhost.localdomain>
2019-10-20 12:49:27 +0200

    required fields

--- login.php
+++ login.php
@@ -1,8 +1,8 @@
 <main>
 <form action="includes/login_inc.php" method="POST">
 <p>Have an account?</p>
-<input type="text" name="username" placeholder="username/email"><br>
-<input type="password" name="password" placeholder="password"><br>
+<input type="text" name="username" placeholder="username/email"required><br>
+<input type="password" name="password" placeholder="password"required><br>
 <input type="submit" name="login" value="Sign_in"> 
 </form>
 </main> 
--- signup.php
+++ signup.php
@@ -1,11 +1,11 @@
 <main>
 <form action="includes/signup_inc.php" method="POST">
 <p>Create your account</p>
-<input type="text" name="email" placeholder="email"><br>
-<input type="text" name="name" placeholder="Full Name"><br>
-<input type="text" name="username" placeholder="Username"><br>
-<input type="password" name="password" placeholder="Password"><br>
-<input type="password" name="repassword" placeholder="Retype Password"><br>
+<input type="text" name="email" placeholder="email" required><br>
+<input type="text" name="name" placeholder="Full Name" required><br>
+<input type="text" name="username" placeholder="Username"required><br>
+<input type="password" name="password" placeholder="Password"required><br>
+<input type="password" name="repassword" placeholder="Retype Password"required><br>
 <input type="radio" name="gender" value="male">Male
 <input type="radio" name="gender" value="female">Female
 <input type="radio" name="gender" value="other">Other<br>
