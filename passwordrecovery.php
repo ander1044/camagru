1cdd1b689748b8c33e0d4e821059dbe4beb5f23a
root <root@localhost.localdomain>
2019-10-20 18:52:23 +0200

    fix me

---
+++ passwordrecovery.php
@@ -0,0 +1,8 @@
+<main>
+<form action="includes/login_inc.php" method="POST">
+<p>Create new password</p>
+<input type="text" name="newpassword" placeholder="New Password"required><br>
+<input type="password" name="retypepassword" placeholder="Retype password"required><br>
+<input type="submit" name="update" value="update"> 
+</form>
+</main> 
