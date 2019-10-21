4fe648c7e917ab96e9da998d2b1328100607e304
root <root@localhost.localdomain>
2019-10-21 22:54:16 +0200

    fix me

--- pic.js
+++ pic.js
@@ -1,27 +1,32 @@
-(function()
-        {
-          var canvas = document.getElementById('canvas'),
-          context = canvas.getContext('2d'),
-          video = document.getElementById('video');
-          vendorUrl = window.URL || window.webkitURL;
+(function(){
 
-          navigator.getMedia = navigator.getUserMedia ||
-          navigator.webkitGetUserMedia || navigator.mozGetUserMedia ||
-          navigator.msGetUserMedia;
+  var defualt_filter = 0;
+  var filters = ['greyscale', 'sepia', 'blur',
+       'brightness', 'contrast', 'hue-rotate', 'hue-rotate2',
+       'hue-rotate3', 'saturate', 'invert', 'none'];
 
-          navigator.getMedia({
-            video : true,
-            audio : false
-          },
-          function(stream)
-          {
-            video.src = vendorUrl.createObjectURL(stream);
-            video.play();
-          },
-          function(error){
-            //An error Occured
-            //error.code
-          });
-          
-        
-        })();
+  var canvas = document.getElementById('canvas'),
+  context = canvas.getContext('2d'),
+  video = document.getElementById('video'),
+  //filter = document.getElementById('filters'),
+  vendorUrl = window.URL || window.webkitURL;
+
+  navigator.getMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
+
+  navigator.getMedia({
+    video: true,
+    audio: false
+  },
+  function(stream){
+    video.src = vendorUrl.createObjectURL(stream);
+    video.play();
+  },
+  function(error){
+    //An error Occured
+    //error.code
+  });
+  document.getElementById('capture').addEventListener('click', function(){
+    context.drawImage(video, 0, 0, 400, 300);
+  });
+  
+})();
