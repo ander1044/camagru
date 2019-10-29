18aee2c8042fbd62f246e87567e3a4bcd23fd144
Anda BEN <anben@c5r6s3.wethinkcode.co.za>
2019-10-29 12:07:03 +0200

    fix me

--- camera.php
+++ camera.php
@@ -16,19 +16,16 @@ require("includes/upload.php");
     <section>
     <div class="booth">
       <form method = "POST" enctype="multipart/form-data">
-        <video id="video" width="400" height="300" autoplay></video>
-        <!-- <a href="captured.php" class="button" id="capture">Take Photo</a> -->
+      <div class="sticker-wrapper">
+        <video id="video" autoplay></video>
+      </div>
         <input id="capture" type="button" value="Take Photo"/>
         
-        <!-- <input type="file" accept="image*/";capture=camera/> -->
         <figure>
           <canvas id="canvas" width="400" height="300">
           </canvas>
-          <!-- <img id="st1" type="hidden" src="https://i.ebayimg.com/images/g/SWMAAOSwoAVbX15m/s-l300.jpg" alt=""> -->
-          <p id="smiley">&#x1F600;</p> 
-          &#x1F923; &#x1F60A; &#x1F970; &#x1F60B;  
+          
         <a href="#" id="btn-download" download="webcam.png" onclick"capture">Download</a>
-        <!-- <input id="retake" type="button" value="retake"/> -->
         <input id="stickers" type="button" value="Add Stickers"/>
         <figcaption>
           <input id="caption" type=text name="caption" placeholder="Say something about this photo..."/>
