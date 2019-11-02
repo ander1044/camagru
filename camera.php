ef2326c7d209d2ada7732b61d4887c80a4c75a63
Anda BEN <anben@c5r6s3.wethinkcode.co.za>
2019-11-02 18:28:47 +0200

    fix me

--- camera.php
+++ camera.php
@@ -8,34 +8,31 @@ require("includes/upload.php");
 <head>
   <meta charset="UTF-8">
   <title>Document</title>
-  <link rel="stylesheet" href="css/filters.css" />
+  <!-- <link rel="stylesheet" href="css/filters.css" /> -->
   <link rel="stylesheet" href="css/main.css" />
   
 </head>
 <body>
-    <section>
-    <div class="booth">
-      <form method = "POST" enctype="multipart/form-data">
-      <div class="sticker-wrapper">
-        <video id="video" autoplay></video>
-      </div>
-        <input id="capture" type="button" value="Take Photo"/>
-        
-        <figure>
-          <canvas id="canvas" width="400" height="300"> 
-          </canvas>
-          <img class="stk3" src=""/>
-        <a href="#" id="btn-download" download="webcam.png" onclick"capture">Download</a>
-        <input id="stickers" type="button" value="Add Stickers"/>
-        <figcaption>
-          <input id="caption" type=text name="caption" placeholder="Say something about this photo..."/>
-        </figcaption>
-        <input id="upload" type="submit" name="fileToUpload" value="Post"/>
-        </figure>
-</form>
-        
-      </div>
-      <script src="capture.js"></script>
-</section>
+    <div class="top-container">
+    <video id="video" autoplay>Something went wrong while streaming</video>
+    <button id="capture">
+    Take Picture
+    </button>
+    <select id="filters">
+    <option value="none">Default</option>
+    <option value="grayscale(100%)">Grayscale</option>
+    <option value="sepia(100%)">Sepia</option>
+    <option value="blur(10px)">Blur</option>
+    <option value="hue-rotate(90deg)">Hue</option>
+    <option value="invert(100%)">Invert</option>
+    <option value="contrast(200%)">contrast</option>
+    </select>
+    <button id="clear">Clear</button>
+    <canvas id="canvas"></canvas>
+    </div>
+    <div class="bottom-container">
+    <div id="thumbnail"></div>
+    </div>
+    <script src="capture.js"></script>
 </body>
 </html>
